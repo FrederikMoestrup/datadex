9c7d1d117cfc143d8556951bc94145b522bc8665
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-12-10 21:37:34 +0100

    Fetching mythical og legendary mangler

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,20 +3,17 @@ package dat;
 import dat.config.ApplicationConfig;
 import dat.config.HibernateConfig;
 import dat.config.Populate;
+import dat.config.PopulateHoenn;
 import dat.dtos.PokemonDTO;
 import dat.services.PokemonService;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
-
-        PokemonDTO pokemon = PokemonService.getPokemon(258); // Fetch Pokémon with ID 1
-        System.out.println(pokemon);
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("datadex");
+        //PopulateHoenn.populate();
+        ApplicationConfig.startServer(7070);
 
     }
 }
 
-
-//EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("datadex");
-//Populate.populate();
-//ApplicationConfig.startServer(7070);
---
+++ src/main/java/dat/config/PopulateHoenn.java
@@ -0,0 +1,32 @@
+package dat.config;
+
+import dat.entities.Pokemon;
+import dat.services.PokemonService;
+import jakarta.persistence.EntityManagerFactory;
+
+public class PopulateHoenn {
+
+    public static void populate() {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("datadex");
+
+        try (var em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            // Fetch Pokémon with IDs from 252 to 386 (Hoenn region Pokémon)
+            for (int id = 252; id <= 386; id++) {
+                // Fetch Pokémon data using the PokemonService
+                var pokemonDTO = PokemonService.getPokemon(id);
+
+                // If the data is valid, persist it
+                if (pokemonDTO != null) {
+                    Pokemon pokemon = new Pokemon(pokemonDTO);
+                    em.persist(pokemon);
+                }
+
+
+            }
+
+            em.getTransaction().commit();
+        }
+    }
+}
