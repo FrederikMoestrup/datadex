cde19b22284874344359d3475b8260432d0fce0b
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-12-10 19:23:51 +0100

    Fetching Egg groups virker

--- src/main/java/dat/services/PokemonSpeciesPartialData.java
+++ src/main/java/dat/services/PokemonSpeciesPartialData.java
@@ -1,13 +1,16 @@
 package dat.services;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true) // Ignore unrecognized fields gracefully
 public class PokemonSpeciesPartialData {
+    @JsonProperty("egg_groups") // Map the "egg_groups" field
     private List<EggGroup> eggGroups;
+
     private Color color;
     private List<FlavorTextEntry> flavorTextEntries;
     private boolean legendary;
