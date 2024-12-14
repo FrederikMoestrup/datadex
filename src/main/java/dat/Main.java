be7042cd5c1d304dda7283b7924c096a1572d415
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-12-14 17:41:21 +0100

    nationaldex

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,15 +1,12 @@
 package dat;
 
-import dat.config.ApplicationConfig;
-import dat.config.HibernateConfig;
-import dat.config.Populate;
-import dat.config.PopulateHoenn;
+import dat.config.*;
 import jakarta.persistence.EntityManagerFactory;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("datadex");
-        //PopulateHoenn.populate();
+        //PopulateNationaldex.populate();
         ApplicationConfig.startServer(7070);
 
     }
