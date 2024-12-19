93ce3b586af1698501f2c05d1f130bd5f5c002d5
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-12-19 09:31:08 +0100

    Update

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,8 +6,8 @@ import jakarta.persistence.EntityManagerFactory;
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("datadex");
-        PopulateNationaldex.populate();
-        PopulateTrainers.populate();
+        //PopulateNationaldex.populate();
+        //PopulateTrainers.populate();
         ApplicationConfig.startServer(7070);
 
     }
